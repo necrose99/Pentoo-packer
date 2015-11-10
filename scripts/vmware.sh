afdd09ba4e292f5b6eb5d7fbbe9685db5dad0d7b
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-09 23:05:43 -0600

    Update vmware.sh

--- scripts/vmware.sh
+++ scripts/vmware.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-# TODO: Install open-vm-tools
+echo "Configuring virtualbox!"
+
+chroot /mnt/pentoo /bin/bash <<'EOF'
+rsync -av -H -A -X --delete-during "rsync://rsync.at.gentoo.org/gentoo-portage/licenses/" "/usr/portage/licenses/"
+
+emerge "=virtual/linux-sources-1" --autounmask-write
+etc-update --automode -5
+emerge "=virtual/linux-sources-1"
+
+emerge ">=app-emulation/open-vm-tools-9.4*" --autounmask-write
+etc-update --automode -5
+emerge ">=app-emulation/open-vm-tools-9.4*"
+
+rc-update add open-vm-tools default
+rc-update add open-vm-tools default
+EOF
